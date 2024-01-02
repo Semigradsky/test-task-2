4bc45fda2fa8b7c1d9ad49af694775f144a3f7e8
Dmitry Semigradsky <semigradskyd@gmail.com>
2024-01-02 13:16:05 +0300

    Use relative pathes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "./",
 	plugins: [svgr(), react(), tsconfigPaths()],
 	server: {
 		open: true,
